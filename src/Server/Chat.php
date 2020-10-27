34167a9385eb3e416826bf4d83deb2179b847258
Alexandre Castelain <alexandre.castelain@fulgens.be>
2020-10-27 21:43:13 +0100

    End the tutorial

--- src/Server/Chat.php
+++ src/Server/Chat.php
@@ -10,6 +10,9 @@ use Ratchet\MessageComponentInterface;
 class Chat implements MessageComponentInterface
 {
     private $clients;
+    private $users = [];
+    private $botName = 'ChatBot';
+    private $defaultChannel = 'general';
 
     public function __construct()
     {
@@ -19,12 +22,18 @@ class Chat implements MessageComponentInterface
     function onOpen(ConnectionInterface $conn)
     {
         $this->clients->attach($conn);
+        $this->users[$conn->resourceId] = [
+            'connection' => $conn,
+            'user' => '',
+            'channels' => []
+        ];
         $conn->send(sprintf('New connection: Hello #%d', $conn->resourceId));
     }
 
     function onClose(ConnectionInterface $closedConnection)
     {
         $this->clients->detach($closedConnection);
+        unset($this->users[$closedConnection->resourceId]);
         echo sprintf('Connection #%d has disconnected\n', $closedConnection->resourceId);
     }
 
@@ -34,20 +43,73 @@ class Chat implements MessageComponentInterface
         $conn->close();
     }
 
-    function onMessage(ConnectionInterface $from, $message)
+    function onMessage(ConnectionInterface $conn, $message)
     {
-        $totalClients = count($this->clients) - 1;
-        echo vsprintf(
-            'Connection #%1$d sending message "%2$s" to %3$d other connection%4$s'."\n", [
-            $from->resourceId,
-            $message,
-            $totalClients,
-            $totalClients === 1 ? '' : 's'
-        ]);
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                $client->send($message);
+        $messageData = json_decode($message);
+        if ($messageData === null) {
+            return false;
+        }
+
+        $action = $messageData->action ?? 'unknown';
+        $channel = $messageData->channel ?? $this->defaultChannel;
+        $user = $messageData->user ?? $this->botName;
+        $message = $messageData->message ?? '';
+
+        switch ($action) {
+            case 'subscribe':
+                $this->subscribeToChannel($conn, $channel, $user);
+                return true;
+            case 'unsubscribe':
+                $this->unsubscribeFromChannel($conn, $channel, $user);
+                return true;
+            case 'message':
+                return $this->sendMessageToChannel($conn, $channel, $user, $message);
+            default:
+                echo sprintf('Action "%s" is not supported yet!', $action);
+                break;
+        }
+        return false;
+    }
+
+    private function subscribeToChannel(ConnectionInterface $conn, $channel, $user)
+    {
+        $this->users[$conn->resourceId]['channels'][$channel] = $channel;
+        $this->sendMessageToChannel(
+            $conn,
+            $channel,
+            $this->botName,
+            $user.' joined #'.$channel
+        );
+    }
+
+    private function unsubscribeFromChannel(ConnectionInterface $conn, $channel, $user)
+    {
+        if (array_key_exists($channel, $this->users[$conn->resourceId]['channels'])) {
+            unset($this->users[$conn->resourceId]['channels']);
+        }
+        $this->sendMessageToChannel(
+            $conn,
+            $channel,
+            $this->botName,
+            $user.' left #'.$channel
+        );
+    }
+
+    private function sendMessageToChannel(ConnectionInterface $conn, $channel, $user, $message)
+    {
+        if (!isset($this->users[$conn->resourceId]['channels'][$channel])) {
+            return false;
+        }
+        foreach ($this->users as $connectionId => $userConnection) {
+            if (array_key_exists($channel, $userConnection['channels'])) {
+                $userConnection['connection']->send(json_encode([
+                    'action' => 'message',
+                    'channel' => $channel,
+                    'user' => $user,
+                    'message' => $message
+                ]));
             }
         }
+        return true;
     }
 }
